e433d0b91a957712e74cad0104e912fb75dfdc24
Pan Ziyue <1763181+d3lta-v@users.noreply.github.com>
2025-04-01 12:07:23 +0800

    Update sstuino-autorepair.sh
    Updated autorepair script to new core name

--- Tooling/sstuino-autorepair.sh
+++ Tooling/sstuino-autorepair.sh
@@ -42,7 +42,7 @@ echo
 
 # Purge original SSTuino installation and AVR cores
 echo "Purging existing cores (safe to ignore errors)..."
-arduino-cli core uninstall "SSTuino II Series Boards:megaavr" || true
+arduino-cli core uninstall "sstuino-ii:megaavr" || true
 arduino-cli core uninstall "arduino:avr" || true
 arduino-cli core uninstall "arduino:megaavr" || true
 
@@ -53,13 +53,13 @@ echo "Reinstalling cores..."
 arduino-cli core update-index --additional-urls https://fourierindustries-llp.github.io/SSTuino_II_Core/package_FourierIndustries-LLP_SSTuino_II_Core_index.json
 arduino-cli core install "arduino:avr"
 arduino-cli core install "arduino:megaavr"
-arduino-cli core install "SSTuino II Series Boards:megaavr" --additional-urls https://fourierindustries-llp.github.io/SSTuino_II_Core/package_FourierIndustries-LLP_SSTuino_II_Core_index.json
+arduino-cli core install "sstuino-ii:megaavr" --additional-urls https://fourierindustries-llp.github.io/SSTuino_II_Core/package_FourierIndustries-LLP_SSTuino_II_Core_index.json
 arduino-cli core upgrade
 
 echo
 
 # Test compile
 echo "Testing compilation of a sample program..."
-arduino-cli compile -b "SSTuino II Series Boards:megaavr:4809" ~/Library/Arduino15/packages/SSTuino\ II\ Series\ Boards/hardware/megaavr/*.*/libraries/WiFiNINA-SSTuino/examples/Ubidots_5_Demo/Ubidots_5_Demo.ino
+arduino-cli compile -b "sstuino-ii:megaavr:4809" ~/Library/Arduino15/packages/sstuino-ii/hardware/megaavr/*.*/libraries/WiFiNINA-SSTuino/examples/Ubidots_5_Demo/Ubidots_5_Demo.ino
 echo "ALL TESTS PASSED, your computer is ready to run Arduino and SSTuino II software!"
 echo "If you still don't see your SSTuino II's port show up in Arduino, please go to 'System Settings > Privacy & Security > Allow accessories to connect' and set it to 'Automatically When Unlocked'"
